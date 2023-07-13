51d289d1e072b3af62c4ad2a1f1c7170ddc37209
Djbrown <offshoresboss@gmail.com>
2023-07-13 21:37:59 +0200

    fix GX Event All

--- gx-event.sh
+++ gx-event.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 cd ~/gx-event
 clear && docker-compose -f docker-compose.cosmiccraft.yml up -d
-sleep 4m
+sleep 150s
 docker cp water/config.yml gx_water:/server/config.yml
+docker cp spigot/spigot.yml gx_lobby:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_01:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_02:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_03:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_04:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_05:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_06:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_07:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_08:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_09:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_10:/data/spigot.yml
