59ad44b8e4799e265567d5c0927f96a4621b454d
Djbrown <offshoresboss@gmail.com>
2023-07-13 22:42:56 +0200

    fix GX Event Script & del permissions script

--- gx_event.sh
+++ gx_event.sh
@@ -1,51 +1,31 @@
 #!/bin/sh
 cd ~/gx-event
-clear && docker-compose -f docker-compose.db.yml up -d
-sleep 30s
-clear && docker-compose -f docker-compose.water.yml up -d
-sleep 30s
-clear && docker cp water/config.yml gx_water:/server/config.yml
-sleep 5s
-clear && docker-compose -f docker-compose.lobby.yml up -d
-sleep 150s
-clear && docker cp spigot/spigot.yml gx_lobby:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team01.yml up -d
-sleep 150s
-clear && docker cp spigot/spigot.yml gx_team_01:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team02.yml up -d
-sleep 150s
-clear && docker cp spigot/spigot.yml gx_team_02:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team03.yml up -d
-sleep 150s
-clear && docker cp spigot/spigot.yml gx_team_03:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team04.yml up -d
-sleep 150s
+clear && docker-compose -f docker-compose.cosmiccraft.yml up -d
+sleep 170s
+docker cp water/config.yml gx_water:/server/config.yml
+docker cp spigot/spigot.yml gx_lobby:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_01:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_02:/data/spigot.yml
+docker cp spigot/spigot.yml gx_team_03:/data/spigot.yml
 docker cp spigot/spigot.yml gx_team_04:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team05.yml up -d
-sleep 150s
 docker cp spigot/spigot.yml gx_team_05:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team06.yml up -d
-sleep 150s
 docker cp spigot/spigot.yml gx_team_06:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team07.yml up -d
-sleep 150s
 docker cp spigot/spigot.yml gx_team_07:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team08.yml up -d
-sleep 150s
 docker cp spigot/spigot.yml gx_team_08:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team09.yml up -d
-sleep 150s
 docker cp spigot/spigot.yml gx_team_09:/data/spigot.yml
-sleep 5s
-clear && docker-compose -f docker-compose.team10.yml up -d
-sleep 150s
 docker cp spigot/spigot.yml gx_team_10:/data/spigot.yml
+sleep 5s
+docker exec gx_water chown -R 1000:1000 /server/config.yml
+docker exec gx_lobby chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_01 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_02 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_03 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_04 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_05 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_06 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_07 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_08 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_09 chown -R 1000:1000 /data/spigot.yml
+docker exec gx_team_10 chown -R 1000:1000 /data/spigot.yml
+sleep 5s
+clear && docker restart gx_db gx_water gx_lobby gx_team_01 gx_team_02 gx_team_03 gx_team_04 gx_team_05 gx_team_06 gx_team_07 gx_team_08 gx_team_09 gx_team_10
